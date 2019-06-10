391e8d216802af983373cbd0e87ca66dc87a51cc
David Foote <david.a.foote@gmail.com>
2019-06-10 17:19:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length >= 1
+    new_array = []
+    counter = 1
+    array.each do |name|
+      new_array.push("#{counter}. #{name}")
+      counter += 1
+   end
+    puts "The line is currently: #{new_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
