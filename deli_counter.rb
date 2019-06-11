289e49001426b2ffba8352ff535e06f48d39aa27
David Foote <david.a.foote@gmail.com>
2019-06-11 16:54:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,19 @@ def line(array)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{position + 1} in line."
+  return name, position
+end
+
+def now_serving(array)
+  if array.empty? == true
+    puts "There is nobody waiting to be served!"
+    elsif array.empty? == false
+    puts "Currently serving #{array.shift}."
+  end
+end
+  
