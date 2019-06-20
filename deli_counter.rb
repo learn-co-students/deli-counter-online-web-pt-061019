2d44ca7c176e36fdeeeacd8cbb3fd5d1254d77ec
Maasa Kono <konomaasa@gmail.com>
2019-06-20 23:40:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(people)
   if people.size == 0
     puts "The line is currently empty."
   else people.size >= 1
-    print "The line is currently: "
+    puts "The line is currently: "
     people.each_with_index do |person, number|
-      print "#{number+1}. #{person} "
+      puts "#{number+1}. #{person} "
     end
   end
 end
