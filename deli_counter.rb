084605470db43fa94472f2b990eca9913bc566d5
Maasa Kono <konomaasa@gmail.com>
2019-06-20 06:15:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+require 'pry'
+def line(people)
+  if people.size == 0
+    puts "The line is currently empty."
+  else people.size >= 1
+    line = "The line is currently: "
+    people.each_with_index do |person, number|
+      order = "#{number+1}. #{person} "
+      puts line << order
+    end
+  end
+end
