72cf41ebafc311838f6c2f844e820e11f2b1393c
Maasa Kono <konomaasa@gmail.com>
2019-06-21 00:18:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,14 @@ def line(people)
   if people.size == 0
     puts "The line is currently empty."
   else people.size >= 1
-    puts "The line is currently: "
+    in_line = "The line is currently:"
     people.each_with_index do |person, number|
-      puts "#{number+1}. #{person} "
+      in_line += " #{number + 1}. #{person}"
     end
+    puts in_line
   end
+end
+
+def take_a_number(line, new_person)
+
 end
