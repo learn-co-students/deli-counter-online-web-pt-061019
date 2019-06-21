6014e2d7655b650dddd19066becb695f63fb77a5
Maasa Kono <konomaasa@gmail.com>
2019-06-21 01:11:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ require 'pry'
 def line(people)
   if people.size == 0
     puts "The line is currently empty."
-  else people.size >= 1
+  else
     in_line = "The line is currently:"
     people.each_with_index do |person, number|
       in_line += " #{number + 1}. #{person}"
@@ -11,6 +11,16 @@ def line(people)
   end
 end
 
-def take_a_number(line, new_person)
+def take_a_number(current_line, new_person)
+  current_line << new_person
+  puts "Welcome, #{new_person}. You are number #{current_line.length} in line."
+end
 
+def now_serving(current_line)
+  if current_line.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{current_line.first}."
+    current_line.shift
+  end
 end
