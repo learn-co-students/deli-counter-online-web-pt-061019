14d6f15efdf4357b50135eb3473b12bcc7615f57
Aida Abdikulova <abdikulova@gmail.com>
2019-06-24 00:45:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+katz_deli=[]
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  elsif katz_deli.size >= 1
+    other_deli.map_with_index do |number, name|
+    puts "The line is currently: #{index+1} #{other_deli}."
+  end
+end
